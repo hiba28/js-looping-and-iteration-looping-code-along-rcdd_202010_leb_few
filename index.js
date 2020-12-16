b8d71a0b9439f153edc53ba851715530f0731a58
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-16 07:25:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
-function writeCards(name , event){
-    for(let i = 0 ; i < name.length ; i++){
+function writeCards(names , event){
+    for(let i = 0 ; i < names.length ; i++){
       return `thank you, ${name[i]}, for the wonderful ${event} gift!` ;
     }
 }
